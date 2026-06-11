ad5efc26c440c248374188bed6293b4a9315e90b
Hidemune Takahashi <h1d3mun3@gmail.com>
2026-06-11 13:26:29 +0000

    Hide Dock icon when Dashboard is closed

--- Avow/AvowApp.swift
+++ Avow/AvowApp.swift
@@ -10,6 +10,7 @@ import SwiftData
 
 @main
 struct AvowApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var delegate
     let modelContainer: ModelContainer
     @State private var appState = AppState()
 
@@ -55,6 +56,39 @@ struct AvowApp: App {
         }
         .defaultSize(width: 900, height: 600)
         .defaultPosition(.center)
+    }
+}
+
+// MARK: - App delegate
+
+final class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        NSApp.setActivationPolicy(.accessory)
+
+        NotificationCenter.default.addObserver(
+            forName: NSWindow.didBecomeKeyNotification,
+            object: nil,
+            queue: .main
+        ) { notification in
+            guard let window = notification.object as? NSWindow,
+                  window.title == "Dashboard" else { return }
+            NSApp.setActivationPolicy(.regular)
+        }
+
+        NotificationCenter.default.addObserver(
+            forName: NSWindow.willCloseNotification,
+            object: nil,
+            queue: .main
+        ) { _ in
+            DispatchQueue.main.async {
+                let hasDashboard = NSApp.windows.contains {
+                    $0.isVisible && $0.title == "Dashboard"
+                }
+                if !hasDashboard {
+                    NSApp.setActivationPolicy(.accessory)
+                }
+            }
+        }
     }
 }
 
