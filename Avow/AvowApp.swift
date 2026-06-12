b23b37131995bfdd6ba430f04bd2c85404fd73fb
Hidemune Takahashi <h1d3mun3@gmail.com>
2026-06-12 14:17:53 +0000

    Always show Dock icon by removing dynamic activation policy

--- Avow/AvowApp.swift
+++ Avow/AvowApp.swift
@@ -10,7 +10,6 @@ import SwiftData
 
 @main
 struct AvowApp: App {
-    @NSApplicationDelegateAdaptor(AppDelegate.self) var delegate
     let modelContainer: ModelContainer
     @State private var appState = AppState()
 
@@ -59,34 +58,6 @@ struct AvowApp: App {
         }
         .defaultSize(width: 900, height: 600)
         .defaultPosition(.center)
-    }
-}
-
-// MARK: - App delegate
-
-final class AppDelegate: NSObject, NSApplicationDelegate {
-    func applicationDidFinishLaunching(_ notification: Notification) {
-        NSApp.setActivationPolicy(.accessory)
-
-        NotificationCenter.default.addObserver(
-            forName: NSWindow.didBecomeKeyNotification,
-            object: nil,
-            queue: .main
-        ) { notification in
-            guard let window = notification.object as? NSWindow,
-                  window.title == "Dashboard" else { return }
-            NSApp.setActivationPolicy(.regular)
-        }
-
-        NotificationCenter.default.addObserver(
-            forName: NSWindow.willCloseNotification,
-            object: nil,
-            queue: .main
-        ) { notification in
-            guard let window = notification.object as? NSWindow,
-                  window.title == "Dashboard" else { return }
-            NSApp.setActivationPolicy(.accessory)
-        }
     }
 }
 
